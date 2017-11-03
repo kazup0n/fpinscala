0d7fcc950e5aa2af6bbf04027447c9f9b01a0294
Kazuhiro Sasaki <dusty.trombone@gmail.com>
2017-11-03 11:22:07 +0900

    exercise 2.5

---
+++ src/main/scala/fpinscala/exercise2_5/package.scala
@@ -0,0 +1,7 @@
+package fpinscala
+
+package object exercise2_5 {
+
+  def compose[A, B, C](f: B=>C, g: A=>B): A=>C = g.andThen(f)
+
+}
---
+++ src/test/scala/fpinscala/exercise2_5/Exercise2_5.scala
@@ -0,0 +1,18 @@
+package fpinscala.exercise2_5
+
+import org.scalatest.FunSuite
+
+class Exercise2_5 extends FunSuite {
+
+
+  test("compose") {
+
+    assert(
+      compose(
+      (str:String) => "** " + str + " **",
+      (name:String) => "Hello, " + name)("World")
+      === "** Hello, World **")
+
+  }
+
+}
