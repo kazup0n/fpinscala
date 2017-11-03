4470a60732a164baf06ccb08f56a44ba85518424
Kazuhiro Sasaki <dusty.trombone@gmail.com>
2017-11-03 13:47:36 +0900

    WorkInProgress in chapter3

---
+++ src/main/scala/fpinscala/exercise3/List.scala
@@ -0,0 +1,69 @@
+package fpinscala.exercise3
+
+import scala.annotation.tailrec
+
+sealed class List[+A]
+
+case object Nil extends List[Nothing]
+
+case class Cons[+A] (head:A, tail:List[A]) extends List
+
+object List {
+
+  def init[A](list: List[A]):List[A] = list match {
+    case Nil => Nil
+    case Cons(x:A, Cons(_:A, Nil)) => List(x)
+    case Cons(x, xs:List[A]) => Cons(x, init(xs))
+  }
+
+  def append[A](a1: List[A], a2: List[A]):List[A] = a1 match {
+    case Nil => Nil
+    case Cons(h, t) => Cons(h, append(t, a2))
+  }
+
+
+  @tailrec
+  def dropWhile[A](list: List[A], f: A=>Boolean):List[A] = list match {
+    case Nil => Nil
+    case Cons(x:A, xs:List[A]) => if(f(x)) dropWhile(xs, f) else list
+  }
+
+
+  @tailrec
+  def drop[A](list: List[A], i: Int):List[A] = list match {
+    case Nil => Nil
+    case Cons(_, xs:List[A])  =>
+      if(i == 1) xs
+      else drop(xs, i-1)
+  }
+
+
+  def setHead[A](list: List[A], head: A):List[A] = list match {
+    case Nil => List(head)
+    case Cons(_, rest) => Cons(head, rest)
+  }
+
+  @tailrec
+  def sum(ints: List[Int], _sum:Int = 0):Int = ints match {
+    case Nil => _sum
+    case Cons(x:Int, xs:List[Int]) => sum(xs, x + _sum)
+  }
+
+  @tailrec
+  def product(ds: List[Double], _product:Double): Double = ds match {
+    case Nil => _product
+    case Cons(0.0, _) => 0
+    case Cons(x:Double, xs:List[Double]) => product(xs, x * _product)
+  }
+
+  def tail[A](xs:List[A]):List[A] = xs match {
+    case Nil => Nil
+    case Cons(_, rest:List[A]) => rest
+  }
+
+
+  def apply[A](as: A*): List[A] =
+  if (as.isEmpty) Nil
+  else Cons(as.head, apply(as.tail: _*))
+
+}
---
+++ src/test/scala/fpinscala/exercise3/Exercise3.scala
@@ -0,0 +1,51 @@
+package fpinscala.exercise3
+
+import org.scalatest.FunSuite
+
+class Exercise3 extends FunSuite{
+
+  test("exercise 3.1 - test match"){
+    val x = List(1, 2, 3, 4, 5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x:Int, Cons(y:Int, Cons(3, Cons(4, _)))) => x + y
+      case _ => 101
+    }
+    assert(x == 3)
+  }
+
+  test("exercise 3.2 - tail") {
+    assert(List.tail(List(1, 2, 3)) == List(2, 3))
+    assert(List.tail(List(1,2)) != List(2, 3))
+
+    assert(List.tail(List(1)) == Nil)
+    assert(List.tail(Nil) == Nil)
+  }
+
+  test("exercise 3.3 - setHead") {
+    assert(List.setHead(List(1,2,3), 0) == List(0, 2, 3))
+    assert(List.setHead(List(1), 0) == List(0))
+    assert(List.setHead(Nil, 0) == List(0))
+
+    assert(List.setHead(List("A","B","C"), "Z") == List("Z", "B", "C"))
+    assert(List.setHead(List("A"), "Z") == List("Z"))
+    assert(List.setHead(Nil, "Z") == List("Z"))
+  }
+
+  test("exercise 3.4 - drop"){
+    assert(List.drop(List(1, 2, 3, 4), 2) == List(3, 4))
+    assert(List.drop(List(1, 2, 3, 4), 4) == Nil)
+    assert(List.drop(Nil, 5) == Nil)
+  }
+
+  test("exercise 3.5 - dropWhile") {
+    def odd(n:Int):Boolean = (n%2 != 0)
+    assert(List.dropWhile(List(1, 3, 5, 2), odd) == List(2))
+  }
+
+  test("exercise 3.6 - init") {
+    assert(List.init(List(1, 2, 3, 4)) == List(1, 2, 3))
+  }
+
+
+}
