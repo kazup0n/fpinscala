9f02703f94219bfe234ae3c8ad6fa8e9d1ca7c10
Kazuhiro Sasaki <dusty.trombone@gmail.com>
2017-11-03 11:21:11 +0900

    exercise 2.3

---
+++ src/main/scala/fpinscala/exercise2_3/package.scala
@@ -0,0 +1,7 @@
+package fpinscala
+
+package object exercise2_3 {
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =  (a) => (b) => f(a, b)
+
+}
---
+++ src/test/scala/fpinscala/exercise2_3/Exercise2_3.scala
@@ -0,0 +1,13 @@
+package fpinscala.exercise2_3
+
+import org.scalatest.FunSuite
+
+class Exercise2_3 extends FunSuite {
+
+
+  test("curry"){
+    assert(curry((a:Int, b:Int)=> a+b)(100)(10) == 110)
+    assert(curry((a:String, b:String)=> a+b)("Hello, ")("World") == "Hello, World")
+  }
+
+}
