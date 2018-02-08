d91c49c805b46d06a28248c5f5706e19c5012886
Sasaki Kazuhiro <sasaki.kazuhiro@classmethod.jp>
2018-02-08 09:23:34 +0900

    exercise 5.6 flatMapまで

--- src/main/scala/fpinscala/exercise5/Stream.scala
+++ src/main/scala/fpinscala/exercise5/Stream.scala
@@ -32,9 +32,9 @@ trait Stream[+A] {
   }
 
 
-  def toListResursive: List[A] = this match {
+  def toListRecursive: List[A] = this match {
     case Empty => List()
-    case Cons(h, t) => h() :: t().toListResursive
+    case Cons(h, t) => h() :: t().toListRecursive
   }
 
   def toList: List[A] = {
@@ -45,6 +45,34 @@ trait Stream[+A] {
 
     go(this, List()).reverse
   }
+
+
+  def existsRecursive(p: A => Boolean):Boolean = this match {
+    case Cons(h, t) => p(h()) || t().existsRecursive(p)
+    case _  => false
+  }
+
+  def exists(p: A => Boolean):Boolean = foldRight(identity(false))((a, z) => p(a) || z)
+
+  def foldRight[B](z: => B)(f: (A, => B)=>B): B = this match {
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+  def forAll(p: A=> Boolean):Boolean = foldRight(identity(true))((a, memo) => p(a) && memo)
+
+  def headOption:Option[A] = foldRight(None:Option[A])( (h, _) => Some(h))
+
+  //  def map[A, B](as: List[A])(f: A => B): List[B] = foldRight(as, List())((x, xs) => Cons(f(x), xs))
+  def map[B](f: A => B): Stream[B] = foldRight(identity(Empty:Stream[B]))( (a, memo) => Stream.cons(f(a), memo))
+
+  //  def filter[A](as: List[A])(f: A => Boolean): List[A] = foldRight(as, List())((x, xs) => if (f(x)) Cons(x, xs) else xs)
+  def filter(p: A=>Boolean): Stream[A] = foldRight(Empty:Stream[A])((a, memo) => if(p(a)) Stream.cons(a, memo) else memo)
+
+  def flatMap[B](f: A=> Stream[B]) :Stream[B] = foldRight(Empty:Stream[B]) { (a, bs) =>
+    f(a).foldRight(bs)((b, memo) => Stream.cons(b, memo))
+  }
+
 }
 
 case object Empty extends Stream[Nothing]
@@ -58,6 +86,9 @@ object Stream {
     lazy val tail = tl
     Cons(() => head, () => tail)
   }
+
+    //  def append2[A](a1: List[A], a2: List[A]): List[A] = foldLeft(reverse(a1), a2)((a, as) => Cons(a, as))
+  def append[A](as:Stream[A], bs:Stream[A]):Stream[A] = as.foldRight(bs)((a, memo) =>  cons(a, memo))
 
   def empty[A]: Stream[A] = Empty
 
--- src/test/scala/fpinscala/exercise5/StreamTest.scala
+++ src/test/scala/fpinscala/exercise5/StreamTest.scala
@@ -7,7 +7,7 @@ class StreamTest extends FunSuite {
 
   test("exercise 5.1 toList") {
     assert(Stream(1, 2, 3, 4, 5).toList === List(1, 2, 3, 4, 5))
-    assert(Stream(1, 2, 3, 4, 5).toListResursive === List(1, 2, 3, 4, 5))
+    assert(Stream(1, 2, 3, 4, 5).toListRecursive === List(1, 2, 3, 4, 5))
   }
 
   test("exercise 5.2 take") {
@@ -25,5 +25,43 @@ class StreamTest extends FunSuite {
     assert(Stream(1, 2, 3, 4).takeWhile(_ % 2 != 0).toList === List(1))
     assert(Stream(1, 2, 3, 4, 5, 6, 7).takeWhile(_ % 5 != 0).toList === List(1, 2, 3, 4))
   }
+
+  test("exercise 5.4 forAll") {
+    assert(Stream(1, 3, 5, 7).forAll(_ % 2 != 0) === true)
+    assert(Stream(1, 2, 3, 5, 7).forAll(_ % 2 != 0) === false)
+    assert(Stream(0, 2).forAll(_==0) === false)
+    assert(Stream(2).forAll(_==0) === false)
+    assert(Stream(0).forAll(_==0) === true)
+    assert(Stream[Int]().forAll(_ % 2!= 0) === true)
+  }
+
+  test("exercise 5.5 headOption") {
+    assert(Stream(1).headOption == Some(1))
+    assert(Stream(1, 2).headOption == Some(1))
+    assert(Stream().headOption == None)
+
+    assert(Stream(1, 2, 3).drop(3).headOption === None)
+    assert(Stream(1, 2, 3).drop(2).headOption === Some(3))
+  }
+
+  test("exercise 5.6 map") {
+    assert(Stream(1,2,3).map(_.toString).toList === List("1", "2", "3"))
+  }
+
+  test("exercise 5.6 filter") {
+    assert(Stream(1,2,3,4).filter(_%2 == 0).toList === List(2, 4))
+  }
+
+  test("exercise 5.6 append") {
+    assert(Stream.append(Stream(1,2,3,4), Stream(5, 6, 7, 8)).toList === List(1,2,3,4, 5,6,7,8))
+  }
+
+   test("exercise 5.6 flatMap") {
+     //1,2,3,4 => "1,0", "2,0", "3,0" => 1,0,2,0,3,0
+     val ns = Stream(1,2,3).map(_.toString+",0").flatMap(s=>Stream(s.split(","):_*)).toList
+     assert(ns === List("1","0","2","0","3","0"))
+  }
+
+
 
 }
