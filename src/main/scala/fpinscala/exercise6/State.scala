11cb351f423c0c2f0051fdb3405336c5804aad7e
Sasaki Kazuhiro <sasaki.kazuhiro@classmethod.jp>
2018-03-12 22:50:33 +0900

    exercise 6.4

--- src/main/scala/fpinscala/exercise6/State.scala
+++ src/main/scala/fpinscala/exercise6/State.scala
@@ -1,5 +1,65 @@
 package fpinscala.exercise6
 
-class State {
+import scala.annotation.tailrec
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+
+case class SimpleRNG(seed: Long) extends RNG {
+
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5deece66dl + 0xbl) & 0xffffffffffffffffl
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
 
 }
+
+object RNG {
+  def nonNegativeInt(rng: RNG): (Int, RNG) = rng.nextInt match {
+    case (n, r) => {
+      val next = if (n.isNegInfinity) 0
+      else if (n < 0) 0
+      else n
+      (next, r)
+    }
+  }
+
+  def double(rng: RNG): (Double, RNG) = nonNegativeInt(rng) match {
+    case (n, r) => {
+      ((Int.MaxValue - n) / Int.MaxValue, r)
+    }
+  }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = (nonNegativeInt(rng), double(rng)) match {
+    case ((i, next), (d, _)) => {
+      ((i, d), next)
+    }
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = intDouble(rng) match {
+    case ((i, d), next) => ((d, i), next)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = double(rng) match {
+    case (d1, r1) => double(r1) match {
+      case (d2, r2) => double(r2) match {
+        case (d3, r3) => ((d1, d2, d3), r3)
+      }
+    }
+  }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    @tailrec
+    def loop(count: Int, rng: RNG, acc: List[Int]): (List[Int], RNG) = rng.nextInt match {
+      case (n, next) => if (count > 0) loop(count - 1, next, n +: acc) else (acc, rng)
+    }
+
+    loop(count, rng, List())
+  }
+
+}
+
---
+++ src/test/scala/fpinscala/exercise6/StateTest.scala
@@ -0,0 +1,93 @@
+package fpinscala.exercise6
+
+import fpinscala.exercise5.Stream
+import org.scalatest.FunSuite
+
+class StateTest extends FunSuite {
+
+  test("exercise 6.1") {
+
+    case class Result(rng: RNG, value: Option[Int])
+
+    val initialRNG = SimpleRNG(10)
+    val initialResult = Seq(Result(initialRNG, None))
+
+    val firstResult = Stream.ones.take(1000).foldRight(initialResult) { (_, acc) =>
+      RNG.nonNegativeInt(acc.head.rng) match {
+        case (n, nextRng: SimpleRNG) => {
+          assert(n >= 0)
+          assert(n <= Int.MaxValue)
+          Result(nextRng, Some(n)) +: acc
+        }
+      }
+    }
+
+    val secondResult = Stream.from(0).take(1000).foldRight(initialResult) { (_, acc) =>
+      RNG.nonNegativeInt(acc.head.rng) match {
+        case (n, nextRng: SimpleRNG) => {
+          assert(n >= 0)
+          assert(n <= Int.MaxValue)
+          Result(nextRng, Some(n)) +: acc
+        }
+      }
+    }
+
+    assert(firstResult == secondResult)
+
+  }
+
+  test("exercise 6.2 double") {
+    Stream.ones.take(1000).foldRight(SimpleRNG(10)) { (_, acc) =>
+      RNG.double(acc) match {
+        case (n, nextRng: SimpleRNG) => {
+          assert(n >= 0)
+          assert(n <= 1)
+          nextRng
+        }
+      }
+    }
+  }
+
+  test("exercise 6.3 intDouble") {
+    val rng = SimpleRNG(10)
+    (RNG.intDouble(rng), RNG.intDouble(rng)) match {
+      case ((v1, next), (v2, next2)) => {
+        assert(v1 == v2)
+        assert(next == next2)
+      }
+    }
+  }
+
+  test("exercise 6.3 doubleInt") {
+    val rng = SimpleRNG(10)
+    (RNG.doubleInt(rng), RNG.doubleInt(rng)) match {
+      case ((v1, next), (v2, next2)) => {
+        assert(v1 == v2)
+        assert(next == next2)
+      }
+    }
+  }
+
+  test("exercise 6.3 double3") {
+    val rng = SimpleRNG(10)
+    (RNG.double3(rng), RNG.double3(rng)) match {
+      case ((v1, next), (v2, next2)) => {
+        assert(v1 == v2)
+        assert(next == next2)
+      }
+    }
+  }
+
+  test("exercise 6.4 ints") {
+    val r1 = RNG.ints(10)(SimpleRNG(10))
+    val r2 = RNG.ints(10)(SimpleRNG(10))
+    val r3 = RNG.ints(10)(SimpleRNG(100))
+    assert(r1 == r2)
+    assert(r1 != r3)
+    assert(r1._1.size == 10)
+    println(RNG.ints(10)(SimpleRNG(10))._1)
+    println(RNG.ints(10)(SimpleRNG(10))._1)
+    println(RNG.ints(10)(SimpleRNG(10))._1)
+  }
+
+}
