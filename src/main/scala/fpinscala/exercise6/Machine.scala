dccdaefb05790e5aed3db9afdb220b30dd4b066b
Sasaki Kazuhiro <sasaki.kazuhiro@classmethod.jp>
2018-04-25 19:45:24 +0900

    exercise 6.10 WIP

--- src/main/scala/fpinscala/exercise6/Machine.scala
+++ src/main/scala/fpinscala/exercise6/Machine.scala
@@ -9,24 +9,21 @@ case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int) {
 
-  //  /**
-  //    * ロックされた状態の自販機に硬貨を投入すると、スナックが残っている場合はロックが解除される
-  //    * ロックが解除された時状態の自動販売機のハンドルを回すと、スナックが出てきてロックがかかる
-  //    * ロックされた状態でハンドルを回したり、ロックが解除された状態で硬貨を投入したりしてもなにも起こらない
-  //    * スナックが売り切れた自動販売機は入力を全て無視する
-  //    * @param inputs
-  //    * @return 最後に自動販売機の中にある硬貨とスナックの数; (硬貨, スナック)
-  //    */
-  //  def simulateMachine(inputs: List[Input]):State[Machine, (Int, Int)] = inputs.foldRight(State.unit((1,1,)):State[Machine, (Int, Int)]) { (input, state) =>
-  //    state.map { (c, s) =>
-  //      input match {
-  //        case Coin if s > 0 =>
-  //      }
-  //
-  //    }
+  def update(input: Input): State[Machine, (Int, Int)] =
+    State((machine:Machine) => {
+      (machine, (0,0))
+    })
 
-
-  //  }
+  /**
+    * ロックされた状態の自販機に硬貨を投入すると、スナックが残っている場合はロックが解除される
+    * ロックが解除された時状態の自動販売機のハンドルを回すと、スナックが出てきてロックがかかる
+    * ロックされた状態でハンドルを回したり、ロックが解除された状態で硬貨を投入したりしてもなにも起こらない
+    * スナックが売り切れた自動販売機は入力を全て無視する
+    *
+    * @param inputs
+    * @return 最後に自動販売機の中にある硬貨とスナックの数; (硬貨, スナック)
+    */
+  //def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)]
 
 
 }
